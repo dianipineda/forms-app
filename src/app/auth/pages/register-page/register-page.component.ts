c3dbfe46122eed4385f56859a55edef62de3dca4
dianipineda <dianipineda55@gmail.com>
2023-06-14 16:45:54 -0500

    validaciones personalizadas sincronas, asincronas, validaciones de FormControl y validaciones de FormGroup

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
+import { EmailValidator } from 'src/app/shared/validators/email-validator.service';
+/*import {
+  cantBeStrider,
+  firstNameAndLastnamePattern,
+  emailPattern,
+} from 'src/app/shared/validators/validators';*/
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterPageComponent implements OnInit {
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.validatorsService.firstNameAndLastnamePattern
+          ),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        //[new EmailValidator()],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorsService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2'),
+      ],
+    }
+  );
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
 
-  constructor() { }
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myForm.reset(this.person);
   }
 
+  onSave() {
+    if (this.myForm.invalid) {
+      return;
+    }
+    /*console.log(this.myForm.value);
+    this.person = this.myForm.value;
+    console.log(this.person);*/
+
+    //no quiero que en persona me quede guardado en el backend el campo termsAndConditions
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+    console.log(this.person);
+  }
+  //Validaciones:
+
+  isValidField(field: string): boolean {
+    return (
+      this.myForm.controls[field].value == false &&
+      this.myForm.controls[field].pristine == false
+    );
+  }
 }
